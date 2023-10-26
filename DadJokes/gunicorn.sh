e084bb2953c54e59153d7e5f90533ed581eacce3
bryanmartineze <bryanmartineze@gmail.com>
2023-10-26 04:32:25 +0000

    working code

---
+++ DadJokes/gunicorn.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# gunicorn.sh
+gunicorn -b :7000 --access-logfile - --error-logfile - wsgi:app
---
+++ DadJokes/wsgi.py
@@ -0,0 +1,5 @@
+# wsgi.py
+from dadjokes import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
